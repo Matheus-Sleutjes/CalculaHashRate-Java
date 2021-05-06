3169a922eef3b50aa430a4794fb48976ac2e8fa0
Matheus-Sleutjes <83664192+Matheus-Sleutjes@users.noreply.github.com>
2021-05-06 09:53:56 -0400

    Update CalculaHash.java
    finalizado a função 2 (calcular gasto energetico)

--- CalculaHash.java
+++ CalculaHash.java
@@ -8,10 +8,10 @@ public class CalculaHash {
 		System.out.println("==================================================");
 		System.out.println("DIGITE A FUNCAO DESEJADA");
 		System.out.println("1 - CALCULAR O GANHO DE UMA PLACA ESPECIFICA");
-		System.out.println("2 - CALCULAR GASTO ENERGETICO POR MES");
+		System.out.println("2 - CALCULAR GASTO ENERGETICO");
 		System.out.println("3 - CALCULAR TODA A RIG");
-		System.out.println("4 - {EM CONSTRUCAO}");
-		System.out.println("5 - {EM CONSTRUCAO}");
+		System.out.println("4 - VARIAS RIGS");
+		System.out.println("5 - SAIR");
 		System.out.println("==================================================");
 		int OP = ler.nextInt();
 			
@@ -98,13 +98,50 @@ public class CalculaHash {
 				
 			break;
 			case 2:
-				System.out.println("4 - {EM CONSTRUCAO}");
-				sair = false;
+				System.out.println("==================================================");
+				System.out.println("       DIGITE SEU GASTO ENERGETICO EM WATTS");
+				System.out.println("==================================================");
+				float watt = ler.nextFloat();
+				
+				System.out.println("==================================================");
+				System.out.println("       DIGITE O VALOR DO KW/H DA SUA REGIAO");
+				System.out.println("==================================================");
+				float kw = ler.nextFloat();
+				
+				double custoEnergiaDiaDouble = ((watt*24)/1000)*kw;   //calcula o valor em Reais por dia de energia
+				float custoEnergiaDia = (float)custoEnergiaDiaDouble;  // transforma a variavel Double  em Float
+				double custoEnergiaDouble = ((watt*720)/1000)*kw;  //calcula o valor em Reais por mes de energia
+				float custoEnergia = (float)custoEnergiaDouble;  // transforma a variavel Double em Float
+				
+				System.out.println("==================================================");
+				System.out.println("O SEU GASTO DE ENERGIA ");
+				System.out.println("--------------------------------------------------");
+				System.out.println("R$:"+ custoEnergiaDia +" POR DIA");
+				System.out.println("--------------------------------------------------");
+				System.out.println("R$:"+ custoEnergia +" POR MES");
+				System.out.println("--------------------------------------------------");
+				System.out.println("!! OBS: OS VALORES ACIMA SAO UMA ESTIMATIVA !!");
+				System.out.println("==================================================");
+				
+				System.out.println("==================================================");    //começo da função sair 
+				System.out.println("DESEJA FAZER OUTRA OPERACAO ?");
+				System.out.println("DIGITE A OPCAO ABAIXO");
+				System.out.println("1 PARA SIM / 2 PARA NAO");
+				System.out.println("==================================================");
+				int resp = ler.nextInt();
+				if(resp == 2){
+					sair = true;
+				}  //fim da função sair
 			break;
 			case 3:
 				System.out.println("4 - {EM CONSTRUCAO}");
 				sair = false;
 			break;
+			case 4:
+			break;
+			case 5:
+					sair = true; 
+			break;	
 			} // fim switch
 		} // fim while sair	
 	} //fim main
